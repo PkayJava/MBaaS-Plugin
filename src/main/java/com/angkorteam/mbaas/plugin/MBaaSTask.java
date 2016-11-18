dfb20487b7101be323b71c0ff7c5232982750937
Socheat KHAUV <skhauv@dminc.com>
2016-11-18 10:28:22 +0700

    rc impl

--- src/main/java/com/angkorteam/mbaas/plugin/MBaaSTask.java
+++ src/main/java/com/angkorteam/mbaas/plugin/MBaaSTask.java
@@ -5,6 +5,7 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import com.mashape.unirest.request.HttpRequestWithBody;
+import org.apache.commons.io.FileUtils;
 import org.gradle.api.tasks.TaskAction;
 import org.sql2o.Connection;
 import org.sql2o.Query;
@@ -12,6 +13,7 @@ import org.sql2o.Sql2o;
 
 import java.io.Console;
 import java.io.File;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -25,6 +27,12 @@ public class MBaaSTask extends Task {
     @TaskAction
     public void mbaasSync() {
         mbaasServer();
+        getProject().getProjectDir();
+        File source = lookupSource();
+        Collection<File> files = FileUtils.listFiles(source, new String[]{"groovy", "html"}, true);
+        for (File file : files) {
+
+        }
     }
 
     private static void ensureServer(String database) {
@@ -77,20 +85,33 @@ public class MBaaSTask extends Task {
             connection.createQuery("create table IF NOT EXISTS server (_id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(100), address VARCHAR (255), login VARCHAR (255), password VARCHAR(255))").executeUpdate();
             connection.createQuery("CREATE UNIQUE INDEX index_server_name on server(name)").executeUpdate();
             // connection.createQuery("drop table if exists page").executeUpdate();
-            connection.createQuery("create table IF NOT EXISTS page (_id INTEGER PRIMARY KEY AUTOINCREMENT, groovy TEXT, html TEXT, page_id VARCHAR(100), groovy_crc32 VARCHAR(100), html_crc32 VARCHAR(100))").executeUpdate();
-            connection.createQuery("CREATE INDEX index_page_groovy_crc32 on page(groovy_crc32)").executeUpdate();
-            connection.createQuery("CREATE INDEX index_page_html_crc32 on page(html_crc32)").executeUpdate();
+            connection.createQuery("create table IF NOT EXISTS page (_id INTEGER PRIMARY KEY AUTOINCREMENT, html_path VARCHAR(255), groovy_path VARCHAR(255), client_groovy TEXT, client_html TEXT, server_groovy TEXT, server_html TEXT, page_id VARCHAR(100), client_groovy_crc32 VARCHAR(100), server_groovy_crc32 VARCHAR(100), client_html_crc32 VARCHAR(100), server_html_crc32 VARCHAR(100))").executeUpdate();
+            connection.createQuery("CREATE INDEX index_page_server_groovy_crc32 on page(server_groovy_crc32)").executeUpdate();
+            connection.createQuery("CREATE INDEX index_page_server_html_crc32 on page(server_html_crc32)").executeUpdate();
+            connection.createQuery("CREATE INDEX index_page_client_groovy_crc32 on page(client_groovy_crc32)").executeUpdate();
+            connection.createQuery("CREATE INDEX index_page_client_html_crc32 on page(client_html_crc32)").executeUpdate();
+            connection.createQuery("CREATE INDEX index_page_html_path on page(html_path)").executeUpdate();
+            connection.createQuery("CREATE INDEX index_page_groovy_path on page(groovy_path)").executeUpdate();
             connection.createQuery("CREATE INDEX index_page_page_id on page(page_id)").executeUpdate();
             // connection.createQuery("drop table if exists rest").executeUpdate();
-            connection.createQuery("create table IF NOT EXISTS rest (_id INTEGER PRIMARY KEY AUTOINCREMENT, groovy TEXT, rest_id VARCHAR(100), groovy_crc32 VARCHAR(100))").executeUpdate();
-            connection.createQuery("CREATE INDEX index_rest_groovy_crc32 on rest(groovy_crc32)").executeUpdate();
+            connection.createQuery("create table IF NOT EXISTS rest (_id INTEGER PRIMARY KEY AUTOINCREMENT, groovy_path VARCHAR(255), client_groovy TEXT, server_groovy TEXT, rest_id VARCHAR(100), server_groovy_crc32 VARCHAR(100), client_groovy_crc32 VARCHAR(100))").executeUpdate();
+            connection.createQuery("CREATE INDEX index_rest_server_groovy_crc32 on rest(server_groovy_crc32)").executeUpdate();
+            connection.createQuery("CREATE INDEX index_rest_client_groovy_crc32 on rest(client_groovy_crc32)").executeUpdate();
+            connection.createQuery("CREATE INDEX index_rest_groovy_path on rest(groovy_path)").executeUpdate();
             connection.createQuery("CREATE INDEX index_rest_page_id on rest(rest_id)").executeUpdate();
         }
     }
 
     protected String lookupDatabase(String database) {
-        String sqlite = new File(getProject().getProjectDir(), database).getAbsolutePath();
-        return sqlite;
+        return new File(getProject().getProjectDir(), database).getAbsolutePath();
+    }
+
+    protected File lookupSource() {
+        File source = new File(getProject().getProjectDir(), "src/main/java");
+        if (!source.exists()) {
+            source.mkdirs();
+        }
+        return source;
     }
 
     public static void main(String[] args) {
