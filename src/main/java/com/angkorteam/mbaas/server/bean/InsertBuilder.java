857020622bf316ab1df7cdbbbd3b3ef3f4453ad9
Socheat KHAUV <pkayjava@gmail.com>
2016-11-25 12:01:03 +0700

    add querybuilder stub

---
+++ src/main/java/com/angkorteam/mbaas/server/bean/InsertBuilder.java
@@ -0,0 +1,23 @@
+package com.angkorteam.mbaas.server.bean;
+
+/**
+ * Created by socheat on 11/25/16.
+ */
+public class InsertBuilder {
+
+    public InsertBuilder() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void addField(String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void intoTable(String table) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String toSQL() {
+        throw new UnsupportedOperationException();
+    }
+}
